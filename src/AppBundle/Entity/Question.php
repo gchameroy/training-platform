984f4740cee982f1530c8b0d23a6666d21fa7cdf
gael nicolle <gael@MacBook-Pro-de-gael-2.local>
2017-03-09 13:01:44 +0100

    Resolved Absolute path constants __DIR__ and __FILE__ should not be used

--- src/AppBundle/Entity/Question.php
+++ src/AppBundle/Entity/Question.php
@@ -38,9 +38,9 @@ class Question
     /**
      * @var string
      *
-     * @ORM\Column(name="image", type="string", length=255)
+     * @ORM\Column(name="file", type="string", length=255)
      */
-    private $image;
+    private $file;
 
     /**
      * @var \stdClass
@@ -49,17 +49,7 @@ class Question
      */
     private $poolQuestion;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="extension", type="string", length=255,nullable=true)
-     * 
-     */
-    private $extension;
 
-    private $file;
-
-    private $tempFilename;
 
     /**
     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Response", mappedBy="question")
@@ -137,53 +127,28 @@ class Question
     }
 
     /**
-     * Set image
+     * Set file
      *
-     * @param string $image
+     * @param string $file
      *
      * @return Question
      */
-    public function setImage($image)
+    public function setFile($file)
     {
-        $this->image = $image;
+        $this->file = $file;
 
         return $this;
     }
 
     /**
-     * Get image
+     * Get file
      *
      * @return string
      */
-    public function getImage()
+    public function getFile()
     {
-        return $this->image;
+        return $this->file;
     }
-
-    /**
-     * Set extension
-     *
-     * @param string $extension
-     *
-     * @return Video
-     */
-    public function setExtension($extension)
-    {
-        $this->extension = $extension;
-
-        return $this;
-    }
-
-    /**
-     * Get extension
-     *
-     * @return string
-     */
-    public function getExtension()
-    {
-        return $this->extension;
-    }
-
 
 
 
@@ -210,105 +175,6 @@ class Question
     {
         return $this->poolQuestion;
     }
-
-
-
-
-
-
-
-
-    public function getFile()
-    {
-        return $this->file;
-    }
-
-    public function setFile(UploadedFile $file = null)
-    {
-        $this->file = $file;
-
-        if (null !== $this->extension) {
-            $this->tempFilename = $this->extension;
-
-            $this->extension = null;
-            $this->image = null;
-        }
-    }
-
-    /**
-     * @ORM\PrePersist()
-     * @ORM\PreUpdate()
-     */
-    public function preUpload()
-    {
-        if (null === $this->file) {
-            return;
-        }
-
-        $this->extension = $this->file->guessExtension();
-        $this->image = $this->file->getClientOriginalName();
-    }
-
-    /**
-   * @ORM\PostPersist()
-   * @ORM\PostUpdate()
-   */
-    public function upload()
-    {
-        if (null === $this->file) {
-            return;
-        }
-
-        if (null !== $this->tempFilename) {
-            $oldFile = $this->getUploadRootDir().'/'.$this->id.'.'.$this->tempFilename;
-            
-            if (file_exists($oldFile)) {
-                unlink($oldFile);
-            }
-        }
-
-        $this->file->move(
-            $this->getUploadRootDir(), 
-            $this->id.'.'.$this->extension   
-        );
-        
-    }
-
-    /**
-    * @ORM\PreRemove()
-    */
-    public function preRemoveUpload()
-    {
-        $this->tempFilename = $this->getUploadRootDir().'/'.$this->id.'.'.$this->extension;
-    }
-
-    /**
-    * @ORM\PostRemove()
-    */
-    public function removeUpload()
-    {
-        if (file_exists($this->tempFilename)) {
-            unlink($this->tempFilename);
-        }
-
-        $this->poolQuestion->removeQuestion($this);
-    }
-
-    public function getUploadDir()
-    {
-        return 'uploads/questions';
-    }
-
-    protected function getUploadRootDir()
-    {
-        return __DIR__.'/../../../web/'.$this->getUploadDir();
-    }
-
-    public function getWebPath()
-    {
-        return $this->getUploadDir().'/'.$this->getId().'.'.$this->getExtension();
-    }
-
 
 }
 
--- src/AppBundle/Entity/Video.php
+++ src/AppBundle/Entity/Video.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * Video
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
  * @ORM\Entity(repositoryClass="AppBundle\Repository\VideoRepository")
  * @UniqueEntity(fields="title", message="Une vidéo existe déjà avec ce titre.")
  */
-class Video
+class Video extends Controller
 {
     /**
      * @var int
@@ -37,10 +37,10 @@ class Video
     /**
      * @var string
      *
-     * @ORM\Column(name="extension", type="string", length=255)
+     * @ORM\Column(name="file", type="string", length=255)
      * 
      */
-    private $extension;
+    private $file;
 
     /**
      * @var string
@@ -50,20 +50,12 @@ class Video
     private $description;
 
     /**
-    * @ORM\Column(name="alt", type="string", length=255, nullable=true)
-    */
-    private $alt;
-
-    /**
      * @var \stdClass
      *
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\PoolVideo", inversedBy="videos")
      */
     private $poolVideo;
 
-    private $file;
-
-    private $tempFilename;
   
     
 
@@ -107,27 +99,27 @@ class Video
     }
 
     /**
-     * Set extension
+     * Set file
      *
-     * @param string $extension
+     * @param string $file
      *
      * @return Video
      */
-    public function setExtension($extension)
+    public function setFile($file)
     {
-        $this->extension = $extension;
+        $this->file = $file;
 
         return $this;
     }
 
     /**
-     * Get extension
+     * Get file
      *
      * @return string
      */
-    public function getExtension()
+    public function getFile()
     {
-        return $this->extension;
+        return $this->file;
     }
 
     /**
@@ -154,6 +146,8 @@ class Video
         return $this->description;
     }
 
+
+
     /**
      * Set poolVideo
      *
@@ -176,113 +170,6 @@ class Video
     public function getPoolVideo()
     {
         return $this->poolVideo;
-    }
-
-    /**
-    * @param string $alt
-    */
-    public function setAlt($alt)
-    {
-        $this->alt = $alt;
-    }
-
-    /**
-    * @return string
-    */
-    public function getAlt()
-    {
-        return $this->alt;
-    }
-
-    public function getFile()
-    {
-        return $this->file;
-    }
-
-    public function setFile(UploadedFile $file = null)
-    {
-        $this->file = $file;
-
-        if (null !== $this->extension) {
-            $this->tempFilename = $this->extension;
-
-            $this->extension = null;
-            $this->alt = null;
-        }
-    }
-
-    /**
-     * @ORM\PrePersist()
-     * @ORM\PreUpdate()
-     */
-    public function preUpload()
-    {
-        if (null === $this->file) {
-            return;
-        }
-
-        $this->extension = $this->file->guessExtension();
-        $this->alt = $this->file->getClientOriginalName();
-    }
-
-    /**
-   * @ORM\PostPersist()
-   * @ORM\PostUpdate()
-   */
-    public function upload()
-    {
-        if (null === $this->file) {
-            return;
-        }
-
-        if (null !== $this->tempFilename) {
-            $oldFile = $this->getUploadRootDir().'/'.$this->id.'.'.$this->tempFilename;
-            
-            if (file_exists($oldFile)) {
-                unlink($oldFile);
-            }
-        }
-
-        $this->file->move(
-            $this->getUploadRootDir(), 
-            $this->id.'.'.$this->extension   
-        );
-        
-    }
-
-    /**
-    * @ORM\PreRemove()
-    */
-    public function preRemoveUpload()
-    {
-        $this->tempFilename = $this->getUploadRootDir().'/'.$this->id.'.'.$this->extension;
-    }
-
-    /**
-    * @ORM\PostRemove()
-    */
-    public function removeUpload()
-    {
-        if (file_exists($this->tempFilename)) {
-            unlink($this->tempFilename);
-        }
-
-        $this->poolVideo->removeVideo($this);
-    }
-
-    public function getUploadDir()
-    {
-        return 'uploads/video';
-    }
-
-    protected function getUploadRootDir()
-    {
-        return __DIR__.'/../../../web/'.$this->getUploadDir();
-    }
-
-    public function getWebPath()
-    {
-        return $this->getUploadDir().'/'.$this->getId().'.'.$this->getExtension();
     }
 
 }
